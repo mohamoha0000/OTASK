0a8a0944998311af97d9433364ae76df5a505945
mohamoha0000 <mohamedelmaeyouf@gmail.com>
2025-06-15 00:37:04 +0100

    add remember_token of table users

--- code sql database/creat table code.sql
+++ code sql database/creat table code.sql
@@ -5,6 +5,7 @@ CREATE TABLE users (
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     role ENUM('member', 'admin') NOT NULL DEFAULT 'member',
+    remember_token VARCHAR(255) DEFAULT NULL,
     profile_picture TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
